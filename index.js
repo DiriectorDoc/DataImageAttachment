197e286ef52b1a292232315a54f6d2ef111409bd
Diriector_Doc <davijust213@yahoo.ca>
2021-02-16 16:39:46 -0500

    Tweaks

--- index.js
+++ index.js
@@ -1,39 +1,26 @@
 const {MessageAttachment} = require("discord.js")
 
 class DataImageAttachment extends MessageAttachment {
-	
-    constructor(uri, name = null, patchData) {
-        super(makeBuffer(uri, name), name, patchData)
-    }
+	constructor(uri, name = null, patchData) {
+		super(DataImageAttachment.makeBuffer(uri, name), name, patchData)
+	}
 
-    setFile(uri, name = null) {
-        return super.setFile(makeBuffer(uri, name), name)
-    }
+	setFile(uri, name = null) {
+		return super.setFile(DataImageAttachment.makeBuffer(uri, name), name)
+	}
 
-    setName(name) {
-        return super.setName(name)
-    }
+	static makeBuffer(uri, name) {
+		try {
+			let data = /data:(?<mime>[\w\/\-\.]+);(?<encoding>\w+),(?<data>.*)/gm.exec(uri)?.groups,
+				attachment = Buffer.from(data?.data ?? uri, data?.encoding ?? "base64");
 
-    _patch(data) {
-        super._patch(data)
-    }
-
-    get spoiler() {
-        return super.spoiler()
-    }
-
-    toJSON() {
-        return super.toJSON()
-    }
-
-	static makeBuffer = function(uri, name){
-		let data = /data:(?<mime>[\w\/\-\.]+);(?<encoding>\w+),(?<data>.*)/gm.exec(uri)?.groups,
-            attachment = Buffer.from(data?.data ?? uri, data?.encoding ?? "base64");
-
-        if(data?.mime && !name.match(`.${data.mime.split("/")[1]}$`)){
-            console.warn(`Image name: "${name}" does not match data mime type: "${data.mime}"`)
-        }
-		return attachment
+			if (data?.mime && !name.match(`.${data.mime.split("/")[1]}$`)) {
+				console.warn(`Image name: "${name}" does not match data mime type: "${data.mime}"`)
+			}
+			return attachment
+		} catch (e) {
+			return null
+		}
 	}
 }
 
